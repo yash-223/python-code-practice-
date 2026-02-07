6f1d49759d28b5e5671b1e8e5d1feae1d65e4097
Yash Sali <saliyash038@gmail.com>
2026-02-07 22:18:40 +0530

    Added ifelse file

---
+++ If&Else.py
@@ -0,0 +1,49 @@
+print("hello python")
+
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+# a = int (input("Enter a number:"))
+# print("your age is",a)
+
+# # conditional operators
+# # >,<,>=,<=,==,!=
+
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+
+# if(a>=18):
+    
+#   print("you can drive")
+  
+# else:
+#     print("you can not drive")
+
+
+num = int(input("enter a number"))
+
+if(num < 0):
+    print("number is negative")
+    
+elif(num == 0):
+    print("number is zero")
+
+elif(num >= 999):
+    print("its a special number.")
+    
+else:
+    print("number is positive")
+
+
+
+
+
+
+
+
+
+
+
+
+
