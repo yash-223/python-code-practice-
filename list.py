12894cc4debfccf7ae2353159f828c9d89a69146
yash sali <143193345+yash-223@users.noreply.github.com>
2026-02-16 15:30:15 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,30 @@
+# lst1  = [["sakshi", "varsha", 123, "yash"],[ 1,2,3,4,5], ["chemistry,bio, math"]]
+
+# print(lst1)
+
+# # access the element
+
+
+
+# # lst1(append("science"))   #append the element 
+# # lst1(insert(""))
+# print(lst1[0])
+
+
+lst2  = ["jan", "feb", "march", "april", "may", "june", "july", "aug", "sep", "oct"]
+print(lst2)
+
+
+lst3 = ["apple", "grapes", "mango", "banana"]
+lst3.append("strawberry")
+print(lst3)
+
+
+lst4 = ["red", "black", "pink", "green", "yellow"]
+lst4.remove("green")
+print(lst4)
+
+lst5 = ["sakshi", "varsh", "mayuri", "aarti", "neha", "shweta"]
+lst5.len[]
+print(lst5)
+
