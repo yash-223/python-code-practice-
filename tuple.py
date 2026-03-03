2deadd998370a43aac655a963277967e1c5209a3
yash sali <143193345+yash-223@users.noreply.github.com>
2026-03-03 21:59:32 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,30 @@
+# Creating a tuples
+
+numbers = ("sakshi", "mansi","shweta", 3.14, True )
+tuple = (1,2,34,5)
+print(numbers)
+
+
+# Accessing a tuple
+
+print(numbers[2])
+print(numbers+tuple)  # concatation 
+
+
+tup1 = ("sakshi", "yash","vishakha","guru")
+print(sorted(tup1))
+
+# t = (1,2,3,4,5)
+# print(del[t])
+ 
+
+
+
+
+tuple = ("sakshi", 1,2,3, 23.7, True)
+a,b,c,d = tuple
+print(a,b,c,d)
+
+
+
+
