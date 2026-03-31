1ee355b881c3a7b67e86c275075c22042c661d2e
Yash Sali <saliyash038@gmail.com>
2026-03-31 21:09:53 +0530

    Add file

---
+++ list_Q.py
@@ -0,0 +1,22 @@
+# marks =[]
+
+# m1 = input("Enter a marks : ")
+# marks.append(m1)
+# m1 = input("Enter a marks : ")
+# marks.append(m1)
+# m1 = input("Enter a marks : ")
+# marks.append(m1)
+# m1 = input("Enter a marks : ")
+# marks.append(m1)
+# m1 = input("Enter a marks : ")
+# marks.append(m1)
+# m1 = input("Enter a marks : ")
+# marks.append(m1)
+# m1 = input("Enter a marks : ")
+# marks.append(m1)
+
+# marks.sort()
+
+# print(marks)
+
+
