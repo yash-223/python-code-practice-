0bd70f91dcf0d2e63997d01fdb4afa8cf8b7f431
Yash Sali <saliyash038@gmail.com>
2026-04-08 10:26:05 +0530

    Add file

---
+++ listQ1.py
@@ -0,0 +1,45 @@
+# fruits=  []
+
+
+# f1 = input("Enter a fruit : ")
+# fruits.append(f1)
+# f1 = input("Enter a fruit : ")
+# fruits.append(f1)
+# f1 = input("Enter a fruit : ")
+# fruits.append(f1)
+# f1 = input("Enter a fruit : ")
+# fruits.append(f1)
+# f1 = input("Enter a fruit : ")
+# fruits.append(f1)
+# f1 = input("Enter a fruit : ")
+# fruits.append(f1)
+
+# print(fruits)
+
+
+
+
+
+
+
+
+
+
+marks = []
+
+m1 = input("Enter a marks ")
+marks.append(m1)
+m1 = input("Enter a marks ")
+marks.append(m1)
+m1 = input("Enter a marks ")
+marks.append(m1)
+m1 = input("Enter a marks ")
+marks.append(m1)
+m1 = input("Enter a marks ")
+marks.append(m1)
+m1 = input("Enter a marks ")
+marks.append(m1)
+
+marks.sort()
+
+print(marks)
