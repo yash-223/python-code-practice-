794ce735915de9fc4ef8337b422f0e51cb97107c
yash sali <143193345+yash-223@users.noreply.github.com>
2026-04-20 00:09:11 +0530

    Add files via upload

--- problem5.py
+++ problem5.py
@@ -1,6 +1,26 @@
+dict = {}
+
+name = input("Enter name : ")
+lang = input("Enter a language : ")
+dict.update({name : lang})
+name = input("Enter name : ")
+lang = input("Enter a language : ")
+dict.update({name : lang})
+name = input("Enter name : ")
+lang = input("Enter a language : ")
+dict.update({name : lang})
 
 
-a = 34 
-b = 5
 
-print(a % b)
+
+
+
+
+
+
+
+
+
+
+
+print(dict)
